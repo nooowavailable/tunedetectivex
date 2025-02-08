42097ed8b6a4fc5eef4901ab17383762b32208ed
nooowavailable <nowavailable@tutamail.com>
2025-02-08 23:22:10 +0100

    Remove unnecessary permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
