8ab2043c3e44ffce204b6225c8540f7d02f264e5
nooowavailable <nowavailable@tutamail.com>
2025-02-09 16:51:08 +0100

    Revert "Remove unnecessary permissions"
    This reverts commit 42097ed8b6a4fc5eef4901ab17383762b32208ed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1-alpha"
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
