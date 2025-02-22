0246ae9e3237fdebb2ae61cf3debb3a0df14d9e7
nooowavailable <nowavailable@tutamail.com>
2025-02-22 17:16:39 +0100

    better looking monochrome app icon (I think)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.dev.tunedetectivex"
         minSdk = 31
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
